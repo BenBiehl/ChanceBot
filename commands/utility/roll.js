d54644d39c022eaffb22dd4f1f90c6744dfe39c2
BenBiehl <benjaminpbiehl@gmail.com>
2024-12-20 17:20:36 -0600

    roll done and 8ball command

--- commands/utility/roll.js
+++ commands/utility/roll.js
@@ -3,14 +3,19 @@ const { SlashCommandBuilder } = require('discord.js')
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('roll')
-        .setDescription('Rolls random number from 1 to selected number. (Default is 100)')
+        .setDescription('Rolls random number from 1 to selected number (Default is 100).')
         .addIntegerOption(option =>
             option
                 .setName('number')
                 .setDescription('Specify a max number you want to roll to.')),
     async execute(interaction) {
-        var num = interaction.options.getInteger('number') ?? 100;
-        var roll_num = Math.floor(Math.random() * num) + 1;
-        await interaction.reply(`${interaction.user.username} rolls ${roll_num} points(s)`);
+        const inputNumber = interaction.options.getInteger('number');
+        if (inputNumber == null) {
+            var num = 100;
+        } else {
+            var num = (inputNumber >= -100000 && inputNumber <= 100000) ? inputNumber : 100;
+        }
+        var rollNum = Math.floor(Math.random() * num) + 1;
+        await interaction.reply(`${interaction.user.username} rolls ${rollNum} point(s)!`);
     },
 };
